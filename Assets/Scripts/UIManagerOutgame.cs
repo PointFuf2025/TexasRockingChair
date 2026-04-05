ffd0f63b66dea5c3ae0aed3d949953db2ba565e6
PointFuf2025 <nicolas33160@gmail.com>
2026-04-05 19:59:59 +0200

    add controls to main menu

--- Assets/Scripts/UIManagerOutgame.cs
+++ Assets/Scripts/UIManagerOutgame.cs
@@ -7,9 +7,11 @@ public class UIManagerOutgame : MonoBehaviour
     [SerializeField]
     private Button playButton;
     [SerializeField] private Button creditButton;
+    [SerializeField] private Button controlsButton;
     [SerializeField] private Button backButton;
 
     [SerializeField] private Camera creditCamera;
+    [SerializeField] private Camera controlsCamera;
     [SerializeField] private CameraMainMenu camInstance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
@@ -17,7 +19,8 @@ public class UIManagerOutgame : MonoBehaviour
     {
         playButton.onClick.AddListener(this.Play);
         creditButton.onClick.AddListener(this.ShowCredits);
-        backButton.onClick.AddListener(this.ExitCredits);
+        backButton.onClick.AddListener(this.ExitCreditsOrControls);
+        controlsButton.onClick.AddListener(this.ShowControls);
     }
 
     public void ShowCredits() 
@@ -28,16 +31,27 @@ public class UIManagerOutgame : MonoBehaviour
         playButton.gameObject.SetActive(false);
         creditButton.gameObject.SetActive(false);
         backButton.gameObject.SetActive(true);
-
     }
 
-    public void ExitCredits() 
+    public void ShowControls()
+    {
+        camInstance.currentCam.gameObject.SetActive(false);
+        controlsCamera.gameObject.SetActive(true);
+        camInstance.canplay = false;
+        playButton.gameObject.SetActive(false);
+        creditButton.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(true);
+    }
+
+    public void ExitCreditsOrControls() 
     {
         creditCamera.gameObject.SetActive(false);
+        controlsCamera.gameObject.SetActive(false);
         camInstance.currentCam.gameObject.SetActive(true);
         camInstance.canplay = true;
         playButton.gameObject.SetActive(true);
         creditButton.gameObject.SetActive(true);
+        controlsButton.gameObject.SetActive(true);
         backButton.gameObject.SetActive(false);
         camInstance.timer = 0;
     }
